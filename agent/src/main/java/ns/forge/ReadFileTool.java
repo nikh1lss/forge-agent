c57eff7f3340dd557dff28721566e956b394c4fb
Nikhil <nsingh12004@gmail.com>
2026-07-30 13:05:17 +1000

    feat: add ReadFileTool tool class for reading file text given a relative path

---
+++ agent/src/main/java/ns/forge/ReadFileTool.java
@@ -0,0 +1,39 @@
+package ns.forge;
+
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public final class ReadFileTool extends AbstractTool<ReadFileTool.Input> {
+    @JsonTypeName("read_file")
+    @JsonClassDescription(
+            "Read the contents of a given relative file path. "
+                    + "Use this when you want to see what's inside a file. "
+                    + "Do not use this with directory names.")
+    public static class Input {
+        @JsonPropertyDescription("The relative path of a file in the working directory.")
+        public String path;
+    }
+
+    public ReadFileTool() {
+        super(Input.class);
+    }
+
+    @Override
+    public String name() {
+        return "read_file";
+    }
+
+    @Override
+    protected String run(Input input) throws IOException {
+        if (input.path == null || input.path.isBlank()) {
+            throw new IllegalArgumentException("path is required");
+        }
+
+        return Files.readString(Path.of(input.path));
+    }
+}
